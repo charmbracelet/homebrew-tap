8a3f09cee5eb81b4cfaf81217bb2ab93d34bda18
Christian Rocha <christian@charm.sh>
2022-08-09 15:00:24 -0700

    Brew formula update for charm version v0.12.2

--- charm.rb
+++ charm.rb
@@ -5,50 +5,70 @@
 class Charm < Formula
   desc "The Charm Tool and Library 🌟"
   homepage "https://charm.sh/"
-  version "0.12.1"
+  version "0.12.2"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/charmbracelet/charm/releases/download/v0.12.1/charm_0.12.1_Darwin_x86_64.tar.gz"
-      sha256 "8aa93a7fa9718933ac4762dd44d627819184950c2999faa4c4c826e611b6962d"
+    if Hardware::CPU.arm?
+      url "https://github.com/charmbracelet/charm/releases/download/v0.12.2/charm_0.12.2_Darwin_arm64.tar.gz"
+      sha256 "c7c8795039f5c9330f824570baab2dbca3cdadf616308ebfb9a50da562e038d3"
 
       def install
         bin.install "charm"
+        bash_completion.install "completions/charm.bash" => "charm"
+        zsh_completion.install "completions/charm.zsh" => "_charm"
+        fish_completion.install "completions/charm.fish"
+        man1.install "manpages/charm.1.gz"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/charmbracelet/charm/releases/download/v0.12.1/charm_0.12.1_Darwin_arm64.tar.gz"
-      sha256 "99f72b7c1283efa9f00f08b3d76f62dd3f7ad97b10fc7d4a9634d116f9f93247"
+    if Hardware::CPU.intel?
+      url "https://github.com/charmbracelet/charm/releases/download/v0.12.2/charm_0.12.2_Darwin_x86_64.tar.gz"
+      sha256 "2ff85a2c95bb8990fd19ec20228aa05e8e82a35a4c357091e8149c5883d39855"
 
       def install
         bin.install "charm"
+        bash_completion.install "completions/charm.bash" => "charm"
+        zsh_completion.install "completions/charm.zsh" => "_charm"
+        fish_completion.install "completions/charm.fish"
+        man1.install "manpages/charm.1.gz"
       end
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/charmbracelet/charm/releases/download/v0.12.1/charm_0.12.1_linux_armv6.tar.gz"
-      sha256 "ffcb13b9551c923c9fa37864dbdbb64656be7b40572fb648e0edba9c0731f57a"
-
-      def install
-        bin.install "charm"
-      end
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/charmbracelet/charm/releases/download/v0.12.1/charm_0.12.1_linux_arm64.tar.gz"
-      sha256 "4c09e4f30922ab08ced9fa378edea3e33935a5b9a22e1ff9ae6eb42857a3d837"
+      url "https://github.com/charmbracelet/charm/releases/download/v0.12.2/charm_0.12.2_linux_arm64.tar.gz"
+      sha256 "6cd23dc481c0b8166d17e0d5435b28c29b8bab4111639413bc92816d3b16afc2"
 
       def install
         bin.install "charm"
+        bash_completion.install "completions/charm.bash" => "charm"
+        zsh_completion.install "completions/charm.zsh" => "_charm"
+        fish_completion.install "completions/charm.fish"
+        man1.install "manpages/charm.1.gz"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/charmbracelet/charm/releases/download/v0.12.1/charm_0.12.1_linux_x86_64.tar.gz"
-      sha256 "040e44b510ff7326631f18f02f8feaf4010bd512a97e369af855cda0cb100938"
+      url "https://github.com/charmbracelet/charm/releases/download/v0.12.2/charm_0.12.2_linux_x86_64.tar.gz"
+      sha256 "332db794338e529e380a0a75fc00bc850f58dd63cb0352d19ecb316dc5d8dd52"
 
       def install
         bin.install "charm"
+        bash_completion.install "completions/charm.bash" => "charm"
+        zsh_completion.install "completions/charm.zsh" => "_charm"
+        fish_completion.install "completions/charm.fish"
+        man1.install "manpages/charm.1.gz"
+      end
+    end
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/charmbracelet/charm/releases/download/v0.12.2/charm_0.12.2_linux_armv6.tar.gz"
+      sha256 "13aa21459f34bd5feaf54bf6f63349cb2d212e526f20abef96ec78a90ac9ef7a"
+
+      def install
+        bin.install "charm"
+        bash_completion.install "completions/charm.bash" => "charm"
+        zsh_completion.install "completions/charm.zsh" => "_charm"
+        fish_completion.install "completions/charm.fish"
+        man1.install "manpages/charm.1.gz"
       end
     end
   end
